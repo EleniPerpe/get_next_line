46f423e8275716c3509fc57084cda7a8da6c76f2
Eleni Perperidou <eperperi@1-C-9.42heilbronn.de>
2024-04-06 20:26:15 +0200

    whatever

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,50 +6,69 @@
 /*   By: eperperi <eperperi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/24 17:42:34 by eperperi          #+#    #+#             */
-/*   Updated: 2024/04/05 14:39:39 by eperperi         ###   ########.fr       */
+/*   Updated: 2024/04/06 20:20:53 by eperperi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
 #ifndef BUFFER_SIZE
-# define BUFFER_SIZE 13
+# define BUFFER_SIZE 10
 #endif
 
 char	*get_next_line(int fd)
 {
 	int				read_chars;
 	char			*buffer;
-	char			*temp_buffer;
+	char			*next_line;
 	static char		*substring;
+	char			*copy;
 	int				i;
 	int				j;
 
-	temp_buffer = NULL;
+	next_line = NULL;
+	copy = NULL;
 	buffer = (char *)malloc ((BUFFER_SIZE + 1) * sizeof(char));
 	if (buffer == NULL)
 		return (NULL);
-	while ((read_chars = read(fd, buffer, BUFFER_SIZE)) >= 0)
+	read_chars = read(fd, buffer, BUFFER_SIZE);
+	while (read_chars > 0)
 	{
+		copy = buffer;
 		if (substring != NULL)
-			buffer = ft_strjoin(substring, buffer);
-		// buffer[read_chars + 1] = '\0';
-		// if (read_chars == 0)
-		// 	return (free (buffer), NULL);
+			copy = ft_strjoin(substring, copy);
+		printf("Copy : %s\n", copy);
+		printf("%d : (buffer) %s\n", read_chars, buffer);
 		i = 0;
-		while (buffer[i] != '\0' && buffer[i] != '\n')
+		while (copy[i] != '\0' && copy[i] != '\n')
 			i++;
-		substring = ft_substr(buffer, 0, i);
-		temp_buffer = ft_strjoin(temp_buffer, substring);
+		printf("..%c..\n", buffer[i - 1]);
+		// printf("i : %d\n", i);
+		// substring = ft_substr(copy, 0, i + 1);
+		// printf("Sub : %s\n", substring);
+		if (copy[i] == '\n' || copy[i] == '\0')
+		{
+			printf("Next line : %s\n", copy);
+			next_line = ft_strjoin(next_line, copy);
+		}
+		// copy = ft_strjoin(copy, substring);
+		printf("Temp : %s\n", next_line);
 		if ((i - 1) != read_chars)
 		{
-			substring = ft_substr(buffer, i, read_chars);
+			substring = ft_substr(buffer, i + 1, read_chars + 1);
+			printf("Sub : %s\n", substring);
 		}
-		return (temp_buffer);
+		else
+		{
+			substring = copy;
+		}
+		printf("Second part sub : %s\n", substring);
+		read_chars = read(fd, buffer, BUFFER_SIZE);
+		printf("read_chars : %d : %s\n\n\n", read_chars, buffer);
 	}
 	if (read_chars == -1)
 		return (free (buffer), NULL);
-	return (buffer);
+	return (copy);
 }
 
 int	main(void)
@@ -71,7 +90,7 @@ int	main(void)
 		printf("%d : %s\n", counter, new_line);
 		if (new_line == NULL)
 			break ;
-		new_line = NULL;
+		free(new_line);
 		j++;
 	}
 	close(fd);
