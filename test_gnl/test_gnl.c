e82007d5b00fa07e655a0251a2f0d9ffbdcdfde0
Eleni Perperidou <eperperi@1-C-9.42heilbronn.de>
2024-04-06 21:14:07 +0200

    test_still_leaks

--- test_gnl/test_gnl.c
+++ test_gnl/test_gnl.c
@@ -20,52 +20,56 @@ char	*get_next_line(int fd)
 	read_chars = read(fd, buffer, BUFFER_SIZE);
 	while (read_chars > 0)
 	{
-		buffer[read_chars] = '\0'; // Null-terminate buffer after reading
+		buffer[read_chars] = '\0';
 		temp = ft_strjoin(substring, buffer);
-		free(substring); // Free previous substring
+		free(substring);
 		substring = temp;
 		i = 0;
 		while (substring[i] != '\0' && substring[i] != '\n')
 			i++;
 		if (substring[i] == '\n')
 		{
-			next_line = ft_substr(substring, 0, i);
-			temp = ft_substr(substring, i + 1, ft_strlen(substring) - i - 1);
+			next_line = ft_substr(substring, 0, i + 1);
+			temp = ft_substr(substring, i + 1, ft_strlen(substring) - i);
 			free(substring);
 			substring = temp;
+			free (buffer);
 			return (next_line);
 		}
 		read_chars = read(fd, buffer, BUFFER_SIZE);
 	}
-	free(buffer); // Free buffer after reading
-	free(substring); // Free remaining substring
-	if (read_chars == -1)
-		return (NULL);
+	free(buffer);
+	if (substring != NULL && *substring != '\0')
+	{
+		next_line = ft_strdup(substring);
+		free(substring);
+		substring = NULL;
+	}
 	return (next_line);
 }
 
-int	main(void)
-{
-	int		fd;
-	char	*new_line;
-	int		counter;
+// int	main(void)
+// {
+// 	int		fd;
+// 	char	*new_line;
+// 	int		counter;
 
-	counter = 0;
-	fd = open("test.txt", O_RDONLY);
-	if (fd == -1)
-	{
-		perror("Error opening file");
-		return (1);
-	}
-	while (counter < 5)
-	{
-		new_line = get_next_line(fd);
-		counter++;
-		if (new_line == NULL)
-			break ;
-		printf("%d : %s\n", counter, new_line);
-		free(new_line);
-	}
-	close(fd);
-	return (0);
-}
+// 	counter = 0;
+// 	fd = open("test.txt", O_RDONLY);
+// 	if (fd == -1)
+// 	{
+// 		perror("Error opening file");
+// 		return (1);
+// 	}
+// 	while (counter < 5)
+// 	{
+// 		new_line = get_next_line(fd);
+// 		counter++;
+// 		if (new_line == NULL)
+// 			break ;
+// 		printf("%d : %s\n", counter, new_line);
+// 		free(new_line);
+// 	}
+// 	close(fd);
+// 	return (0);
+// }
